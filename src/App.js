8b55cb4475812bb63e3eca6916323e8618a36637
edox <edox61@gmail.com>
2021-01-04 20:04:28 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import TodoList from "./components/TodoList"
+import TodoForm from "./components/TodoForm"
+
+const App = () => {
+    return (
+        <div className="container mt-4">
+            <div className="row">
+                <div className="col-8">
+                    <TodoList />
+                </div>
+                <div className="col-4">
+                    <TodoForm />
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Todo = ({ todo }) => {
+
+    //const { todo } = props;
+
+    return (
+
+        <div className="card mt-2">
+            <div className="card-body border border-primary">
+                <h3 className="card-title text-right">
+                    {todo.title}
+                    <button className="btn btn-sm btn-outline-success ml-2">
+                        Terminar
+                    </button>
+                </h3>
+                <p className="card-text text-right">
+                    {todo.description}
+                </p>
+                <hr color="blue" />
+
+                <div className="d-flex justify-content-end">
+                    <button className=" btn btn-sm btn-outline-primary mr-2">
+                        Editar
+                    </button>
+                    <button className="btn btn-sm btn-outline-danger">
+                        Eliminar
+                    </button>
+                </div>
+
+            </div>
+        </div>
+
+
+    )
+}
+
+export default Todo
