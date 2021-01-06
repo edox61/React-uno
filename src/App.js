1c371219d59326404333980a9ba066403224b0df
edolf <edox61@gmail.com>
2021-01-05 22:47:48 -0300

    Objeto sube hasta App y se pasan valores desde App hasta TodoList mediante props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TodoList from "./components/TodoList"
 import TodoForm from "./components/TodoForm"
 
+const initialTodos = [
+    {
+        id: 1,
+        title: "todo #1",
+        description: "Desc del todo #1",
+        completed: false
+    },
+
+    {
+        id: 2,
+        title: "todo #2",
+        description: "Desc del todo #2",
+        completed: false
+    }
+
+]
+
 const App = () => {
+
+    const [todos, setTodos] = useState(initialTodos)
+
     return (
         <div className="container mt-4">
             <div className="row">
                 <div className="col-8">
-                    <TodoList />
+                    <TodoList todos={todos} />
                 </div>
                 <div className="col-4">
                     <TodoForm />
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,48 +1,14 @@
 import React, { useState } from 'react'
 import Todo from "./Todo"
 
-/*
-const todo1 = {
-    id: 1,
-    title: "todo #1",
-    description: "Desc del todo #1",
-    completed: false
-}
-
-const todo2 = {
-    id: 2,
-    title: "todo #2",
-    description: "Desc del todo #2",
-    completed: false
-}
-*/
-
-const initialTodos = [
-    {
-        id: 1,
-        title: "todo #1",
-        description: "Desc del todo #1",
-        completed: false
-    },
-
-    {
-        id: 2,
-        title: "todo #2",
-        description: "Desc del todo #2",
-        completed: false
-    }
-
-]
 
 
-const TodoList = () => {
-
-    const [todo, setTodos] = useState(initialTodos)
+const TodoList = ({ todos }) => {
 
     return (
         <div>
             <h1>Soy Todo List</h1>
-            { todo.map(todo => <Todo todo={todo} key={todo.id} />)}
+            { todos.map(todo => <Todo todo={todo} key={todo.id} />)}
 
         </div >
     )
