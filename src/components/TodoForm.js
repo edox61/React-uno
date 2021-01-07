0643fc6215cb9b62a200a8d64431d8c773d8243b
edolf <edox61@gmail.com>
2021-01-07 17:48:10 -0300

    se crea form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TodoForm = () => {
+
+    const initialFromValues = {
+        title: '',
+        description: ''
+    }
+
+    const [fromValues, setFromValues] = useState(initialFromValues)
+    const { title, description } = fromValues;
+
     return (
         <div>
-            <h1>Soy TodoForm</h1>
+            <h1>Nueva Tarea</h1>
+            <form>
+
+                <input
+                    type="text"
+                    placeholder="Titulo"
+                    className="form-control"
+                    value={title}
+                />
+
+                <textarea
+                    placeholder="Descripcion"
+                    className="form-control mt-2"
+                    value={description}
+                />
+
+                <button
+                    className="btn btn-block btn-primary mt-2" >
+                    Agregar Tarea
+                </button>
+
+            </form>
         </div>
     )
 }
